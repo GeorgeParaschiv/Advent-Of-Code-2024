25c7e47c64738a86a41b9b06b2371560ea26b95d
GeorgeParaschiv <gparaschiv10@gmail.com>
2024-12-17 05:47:30 -0500

    Day 15

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -3,8 +3,10 @@
 # Date: 2024-12-15
 
 import time
+from collections import deque, defaultdict
 
 DIRECTIONS = {"^": (-1, 0), ">": (0, 1), "v": (1, 0), "<": (0, -1)}
+BOX = {"[", "]"}
 
 # Parse Input
 def parseInput():
@@ -18,13 +20,13 @@ def parseInput():
         
     return grid, movements
 
-def printGrid(grid, MAXCOL):
-    for row, col in grid.keys():
-        
-        if (col < MAXCOL-1):
-            print(grid[(row, col)], end="")
-        else:
-            print(grid[(row, col)])        
+def printGrid(grid):
+    
+    for row in grid:
+        for col in row:
+            print(col, end="")
+        print() 
+    print()      
 
 # Q1 : 
 def q1():
@@ -33,59 +35,58 @@ def q1():
     start = time.perf_counter()
     
     
-    grid = {}
+    grid = input[0]
     movements = input[1]
-    MAXCOL = len(input[0][0])
     
-    for rowIndex in range(len(input[0])):
-        for colIndex in range(len(input[0][0])):
-            grid[(rowIndex, colIndex)] = input[0][rowIndex][colIndex]
-            
-            if input[0][rowIndex][colIndex] == '@':
-                x, y = rowIndex, colIndex                  
+    for rowIndex in range(len(grid)):
+        for colIndex in range(len(grid[0])):            
+            if grid[rowIndex][colIndex] == '@':
+                x, y = rowIndex, colIndex 
+                break                 
+   
     
     for movement in movements:
         
         dx, dy = DIRECTIONS[movement]
+        nx, ny = x + dx, y + dy
         
-        if (grid[(x+dx, y+dy)] == "#"):
-                    
-            # print("Move " + movement)     
-            # printGrid(grid, MAXCOL)
-            # print()
-            
+        if (grid[nx][ny] == "#"):            
             continue
-        elif (grid[(x+dx, y+dy)] == "."):
-            grid[(x,y)] = "."
-            grid[(x+dx, y+dy)] = "@"            
+        elif (grid[nx][ny] == "."):
+            grid[x][y] = "."
+            grid[nx][ny] = "@"            
         else:
-            nx, ny = x + 2*dx, y + 2*dy
+            nx, ny = nx + dx, ny + dy
             
-            while(grid[(nx, ny)] == "O"):
+            while(grid[nx][ny] == "O"):
                 nx, ny = nx + dx, ny + dy
                              
-            if (grid[(nx, ny)] == "#"):
+            if (grid[nx][ny] == "#"):
                 continue
             else:
-                grid[(nx, ny)] = "O"
-                grid[(x+dx, y+dy)] = "@"
-                grid[(x,y)] = "."
+                grid[nx][ny] = "O"
+                grid[x + dx][y + dy] = "@"
+                grid[x][y] = "."
         
         x,y = x+dx, y+dy
-         
-        # print("Move " + movement)     
-        # printGrid(grid, MAXCOL)
-        # print()
     
     total = 0 
-    for x,y in grid.keys():
-        if (grid[(x, y)] == "O"):
-            total += (x * 100 + y)           
-
-    
+    for x in range(len(grid)):
+        for y in range(len(grid[0])): 
+            if (grid[x][y] == "O"):
+                total += (x * 100 + y)           
 
     elapsed = (time.perf_counter() - start) * 1000000
     return total, round(elapsed)
+
+def performMovement(grid, tree, x, y, dx, index):   
+    
+    for nx, ny in tree[(x, y)]:
+        performMovement(grid, tree, nx, ny, dx, index)
+    
+    if (grid[x][y] == "["):  
+        grid[x+dx][y], grid[x+dx][y+1] = grid[x][y], grid[x][y+1]    
+        grid[x][y], grid[x][y+1] = ".", "."  
 
 # Q2 : 
 def q2():
@@ -93,7 +94,113 @@ def q2():
     input = parseInput()
     start = time.perf_counter()
     
-    total = 0
+    grid = input[0]
+    temp = []
+    movements = input[1]
+    
+    # Remake Grid and find Start
+    for rowIndex in range(len(grid)):
+        temp.append([])
+        for colIndex in range(len(grid[0])):
+            if grid[rowIndex][colIndex] == 'O':
+                temp[rowIndex].append("[") 
+                temp[rowIndex].append("]")
+            
+            elif grid[rowIndex][colIndex] == '@':
+                temp[rowIndex].append("@")
+                temp[rowIndex].append(".")
+                x, y = rowIndex, colIndex*2   
+                
+            else:
+                temp[rowIndex].append(grid[rowIndex][colIndex])
+                temp[rowIndex].append(grid[rowIndex][colIndex])
+    grid = temp
+     
+    # Iterate Through Movements          
+    for index, movement in enumerate(movements):
+        
+        #print("Moving " + movement)        
+        dx, dy = DIRECTIONS[movement]
+        nx, ny = x + dx, y + dy
+        
+        if (grid[nx][ny] == "#"):
+            continue
+        elif (grid[nx][ny] == "."):
+            grid[x][y] = "."
+            grid[nx][ny] = "@"          
+        else:
+            
+            # If Pushing Right or Left
+            if (dx == 0):
+                ny += dy
+                
+                while(grid[nx][ny] in BOX):
+                    ny += dy
+                                
+                if (grid[nx][ny] == "#"):
+                    continue
+                else:
+                    for i in range(ny, y, -dy):
+                        grid[nx][i] = grid[nx][i-dy]
+                    grid[nx][y + dy] = "@"
+                    grid[nx][y] = "."
+                    
+            # Moving Up or Down
+            else:  
+                # Create Root of Tree
+                if (grid[nx][ny] == "]"):
+                    ny -= 1
+                      
+                tree = defaultdict(list)
+                que = deque([(nx, ny)])
+                rootx, rooty = nx, ny
+                possible = True
+                
+                while(que):
+                
+                    nx, ny = que.popleft()
+                    
+                    # #        #      []    []
+                    # []  or  []  or  #  or  #
+                    if grid[nx+dx][ny] == "#" or grid[nx+dx][ny+1] == "#":
+                        possible = False
+                        break
+                    
+                    # []
+                    # []
+                    if (grid[nx+dx][ny] == "["): 
+                        tree[(nx, ny)].append((nx+dx, ny))
+                        que.append((nx+dx, ny))
+                    else:
+                        
+                        # [].       []
+                        #  []  or  [].
+                        if (grid[nx+dx][ny] == "]"): 
+                            if (nx+dx, ny-1) not in tree[(nx, ny-2)]:
+                                tree[(nx, ny)].append((nx+dx, ny-1))
+                                que.append((nx+dx, ny-1))
+                                
+                        # .[]      []
+                        # []   or  .[]     
+                        if (grid[nx+dx][ny+1] == "["): 
+                            tree[(nx, ny)].append((nx+dx, ny+1))
+                            que.append((nx+dx, ny+1))        
+                
+                if (possible):
+                    performMovement(grid, tree, rootx, rooty, dx, index)
+                    grid[x][y] = "."
+                    grid[x + dx][y + dy] = "@"
+                    x, y = x + dx, y + dy  
+
+                continue
+                        
+        x, y = x + dx, y + dy 
+    
+    total = 0 
+    for x in range(len(grid)):
+        for y in range(len(grid[0])): 
+            if (grid[x][y] == "["):
+                total += (x * 100 + y)      
 
     elapsed = (time.perf_counter() - start) * 1000000
     return total, round(elapsed)
